1b982c3f75b03076692206c7a528ae5bd713163a
Ayeeta <elijate@gmail.com>
2017-07-14 13:42:18 +0300

    add static files

--- calc_100ys.py
+++ calc_100ys.py
@@ -1,11 +1,11 @@
 def calc_100ys(name, age):
     """ Function calculates the year you will be 100yo """
+    
     future_age = 0
-    if isinstance(age,int):
+    if isinstance(age, int):
         future_age = (100 - age) + 2017 + future_age
     return "{}, the year you will be 100 years will be {}".format(name, future_age)
+            
+  
 
-
-print(calc_100ys("Elijah", 25))  
-
-
+print(calc_100ys("Elijah", 25))
---
+++ tempdrjahrzfjb.py
@@ -0,0 +1,5 @@
+name = input("Enter Your Name: ")
+age = int(input("Enter your age "))
+future_age = 100 - age
+years = 2017 + future_age
+print("{} you will be 100 years in {}".format(name, years))
